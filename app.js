27a15ba060353d8c5fac7e6854395072a84b1242
meeschka <michpitts@hotmail.com>
2019-06-02 14:08:58 -0400

    Initial setup, added express and ejs, added home route

---
+++ app.js
@@ -0,0 +1,14 @@
+var express = require("express");
+var app = express();
+var port = 3000;
+
+app.set("view engine", "ejs");
+
+app.get("/", function(req, res){
+  res.render("landing");
+});
+
+
+app.listen(port, ()=>{
+    console.log("Server has started");
+});
