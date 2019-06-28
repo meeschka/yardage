02a32e632668df27d8e42d10cfec0cb905bb16f3
meeschka <michpitts@hotmail.com>
2019-06-27 20:56:12 -0400

    Fixed navbar for lighter look

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var app = express();
 var port = 3000;
 
 app.set("view engine", "ejs");
-
+app.use(express.static(__dirname+"/public"));
 app.get("/", function(req, res){
   res.render("landing");
 });
