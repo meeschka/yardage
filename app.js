a7b38513ae01279904ac604d3fcb0c1677a52037
meeschka <michpitts@hotmail.com>
2019-06-29 22:40:07 -0400

    Cleaned up landing page and header, added connection to fabric page

--- app.js
+++ app.js
@@ -4,9 +4,46 @@ var port = 3000;
 
 app.set("view engine", "ejs");
 app.use(express.static(__dirname+"/public"));
+
 app.get("/", function(req, res){
   res.render("landing");
 });
+
+app.get("/fabrics", function(req, res){
+  var tempFabricsArray = [
+  {
+    name: "Grey wool",
+    image: "/assets/fabric1.jpg",
+    description: "A grey wool I purchased in Paris"
+  },
+  {
+    name: "Cotton broadcloth",
+    image: "/assets/fabric2.jpg",
+    description: "A bunch of basic white cotton broadcloth, stocked up at a clearance sale"
+  },
+  {
+    name: "Neon pink silk",
+    image: "/assets/pinkfabric.jpg",
+    description: "It's so bright and fun!"
+  },
+  {
+    name: "Grey wool",
+    image: "/assets/fabric1.jpg",
+    description: "A grey wool I purchased in Paris"
+  },
+  {
+    name: "Cotton broadcloth",
+    image: "/assets/fabric2.jpg",
+    description: "A bunch of basic white cotton broadcloth, stocked up at a clearance sale"
+  },
+  {
+    name: "Neon pink silk",
+    image: "/assets/pinkfabric.jpg",
+    description: "It's so bright and fun!"
+  }
+];
+  res.render("fabrics/index", {fabrics: tempFabricsArray});
+})
 
 
 app.listen(port, ()=>{
