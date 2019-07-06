bb023249af479fde90bb39672d6415d24b80c11e
meeschka <michpitts@hotmail.com>
2019-07-05 20:13:53 -0400

    added post route to add new fabrics, created basic form to enter new data

--- app.js
+++ app.js
@@ -1,50 +1,72 @@
 var express = require("express");
+var bodyParser = require("body-parser");
+
 var app = express();
 var port = 3000;
 
+
+app.use(bodyParser.urlencoded({extended:true}));
+
 app.set("view engine", "ejs");
 app.use(express.static(__dirname+"/public"));
+
+var tempFabricsArray = [
+{
+  name: "Grey wool",
+  image: "/assets/fabric1.jpg",
+  description: "A grey wool I purchased in Paris"
+},
+{
+  name: "Cotton broadcloth",
+  image: "/assets/fabric2.jpg",
+  description: "A bunch of basic white cotton broadcloth, stocked up at a clearance sale"
+},
+{
+  name: "Neon pink silk",
+  image: "/assets/pinkfabric.jpg",
+  description: "It's so bright and fun!"
+},
+{
+  name: "Grey wool",
+  image: "/assets/fabric1.jpg",
+  description: "A grey wool I purchased in Paris"
+},
+{
+  name: "Cotton broadcloth",
+  image: "/assets/fabric2.jpg",
+  description: "A bunch of basic white cotton broadcloth, stocked up at a clearance sale"
+},
+{
+  name: "Neon pink silk",
+  image: "/assets/pinkfabric.jpg",
+  description: "It's so bright and fun!"
+}
+];
 
 app.get("/", function(req, res){
   res.render("landing");
 });
 
 app.get("/fabrics", function(req, res){
-  var tempFabricsArray = [
-  {
-    name: "Grey wool",
-    image: "/assets/fabric1.jpg",
-    description: "A grey wool I purchased in Paris"
-  },
-  {
-    name: "Cotton broadcloth",
-    image: "/assets/fabric2.jpg",
-    description: "A bunch of basic white cotton broadcloth, stocked up at a clearance sale"
-  },
-  {
-    name: "Neon pink silk",
-    image: "/assets/pinkfabric.jpg",
-    description: "It's so bright and fun!"
-  },
-  {
-    name: "Grey wool",
-    image: "/assets/fabric1.jpg",
-    description: "A grey wool I purchased in Paris"
-  },
-  {
-    name: "Cotton broadcloth",
-    image: "/assets/fabric2.jpg",
-    description: "A bunch of basic white cotton broadcloth, stocked up at a clearance sale"
-  },
-  {
-    name: "Neon pink silk",
-    image: "/assets/pinkfabric.jpg",
-    description: "It's so bright and fun!"
-  }
-];
+
   res.render("fabrics/index", {fabrics: tempFabricsArray});
 })
 
+app.post("/fabrics", function(req, res){
+  //get data from form, add to fabrics db
+  var newFabric = {
+    name: req.body.name,
+    image: req.body.image,
+    description: req.body.description
+  }
+  tempFabricsArray.push(newFabric);
+  res.redirect("/fabrics");
+  //redirect back to fabrics
+})
+
+app.get("/fabrics/new", function(req, res){
+  res.render("fabrics/new");
+})
 
 app.listen(port, ()=>{
     console.log("Server has started");
